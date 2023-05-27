5bb323bc168ed922abe6595c2907aaff90869355
Leonardo Tolaba <leonardotolaba.20@gmail.com>
2023-05-27 12:20:34 -0300

    Solucionando el acceso al login 4

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,7 +1,6 @@
-
-const url = ( window.location.hostname.includes("localhost") )
-            ? "http://localhost:8080/api/auth/"
-            : "https://appchat-production-6bee.up.railway.app/";
+const url = window.location.hostname.includes("localhost")
+  ? "http://localhost:8080/api/auth/"
+  : "https://appchat-production-6bee.up.railway.app/";
 
 let user = null;
 let socket = null;
@@ -23,42 +22,38 @@ const validarJWT = async () => {
     window.location = "index.html";
     throw new Error("No hay token en el servidor");
   }
+  const resp = await fetch(url, {
+    headers: { "x-token": token },
+  });
+
+  // const { user: userDB, token: tokenDB } = await resp.json();
+  const data = await resp.json();
 
 
-  
-    const resp = await fetch(url
-      , {
-      headers: { "x-token": token },
-    });
-    const data = await resp.json();
-    console.log(data)
-    console.log(typeof(data))
-    const { user: userDB, token: tokenDB } = data;
-  
-    console.log(userDB, tokenDB);
-    //Seteamos el token renovado
-    try {
+  console.log(data);
+  //Seteamos el token renovado
+  try {
     localStorage.setItem("token", tokenDB);
     user = userDB;
     document.title = user.nombre;
 
     await conectarSocket();
   } catch (error) {
-    console.log("Error en el try",error);
-    // window.location = "index.html";
+    console.log("Error en el try", error);
+    window.location = "index.html";
   }
 };
 
-const conectarSocket = async() => {
+const conectarSocket = async () => {
   //Conexion con el backend
   socket = io({
     //parametros para enviar al websocket en la documentacion
-    "extraHeaders": {
+    extraHeaders: {
       "x-token": localStorage.getItem("token"),
-    }
+    },
   });
 
-  console.log("2- ", socket)
+  console.log("2- ", socket);
 
   socket.on("connect", () => {
     console.log("Sockets Online");
@@ -69,69 +64,69 @@ const conectarSocket = async() => {
   });
 
   socket.on("recibir-mensajes", dibujarMensajes);
-  socket.on("usuarios-activos",dibujarUsuarios);
+  socket.on("usuarios-activos", dibujarUsuarios);
   socket.on("mensaje-privado", dibujarMensajesPrivados);
 };
 
-
-const dibujarUsuarios = (usuarios =[]) => {
+const dibujarUsuarios = (usuarios = []) => {
   //construimos el html
   let usersHtml = "";
-  usuarios.forEach( user => {
-    usersHtml+=`
+  usuarios.forEach((user) => {
+    usersHtml += `
     <li>
       <p>
         <h5 class="text-success"> ${user.nombre}</h5>
         <span class="fs-6 text-muted">${user.uid}</span>
       </p>
-    </li>`
+    </li>`;
   });
   ulUsuarios.innerHTML = usersHtml;
-}
+};
 
-
-const dibujarMensajes = (mensajes =[]) => {
+const dibujarMensajes = (mensajes = []) => {
   //construimos el html
   let mensajesHtml = "";
-  mensajes.forEach( ({nombre, mensaje}) => {
-    mensajesHtml+=`
+  mensajes.forEach(({ nombre, mensaje }) => {
+    mensajesHtml += `
     <li>
       <p>
         <span class="text-primary"> ${nombre}:</span>
         <span class="fs-6 text-muted">${mensaje}</span>
       </p>
-    </li>`
+    </li>`;
   });
   ulMensajes.innerHTML = mensajesHtml;
-}
+};
 
 const dibujarMensajesPrivados = (payload) => {
-  let {de, msg} = payload;
+  let { de, msg } = payload;
   // construimos el html
   let mensajesHtml = "";
-  mensajesHtml+=`
+  mensajesHtml += `
     <li>
       <p>
         <span class="text-info fw-bold"> ${de}:</span>
         <span class="bg-info text-white p-1 rounded">${msg}</span>
       </p>
-    </li>`
-    ulMensajesPrivados.innerHTML = mensajesHtml;
-}
+    </li>`;
+  ulMensajesPrivados.innerHTML = mensajesHtml;
+};
 
-
-txtMensaje.addEventListener("keyup", ({keyCode}) => {
+txtMensaje.addEventListener("keyup", ({ keyCode }) => {
   const msg = txtMensaje.value;
   const uid = txtUid.value;
-  if( keyCode !== 13){return;}
+  if (keyCode !== 13) {
+    return;
+  }
 
-  if(msg.length == 0){return;}
+  if (msg.length == 0) {
+    return;
+  }
 
-  socket.emit("enviar-mensaje", {msg, uid});
+  socket.emit("enviar-mensaje", { msg, uid });
 
-  txtMensaje.value = ""
-
-})
+  txtMensaje.value = "";
+});
 
 const main = async () => {
   //Tengo que validar el JWT
